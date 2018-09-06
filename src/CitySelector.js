e8f6b0ce1b015d0bc62dd0b38254f304669bc5ba
Carlos Zúñiga <carlos.zuniga@redbus.com>
2018-09-06 18:39:11 -0500

    Show description for selection

--- src/CitySelector.js
+++ src/CitySelector.js
@@ -4,12 +4,25 @@ import Header from './Header';
 import './CitySelector.css';
 
 export default class CitySelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: null,
+      showAbout: false,
+    }
+  }
+
   static alive() {
     return (<img src="img/heart-full.png" height="20" alt="life"/>);
   }
 
   static dead() {
     return (<img src="img/heart-empty.png" height="20" alt="life"/>);
+  }
+
+  button(cityName) {
+    return <button className={this.state.city === cityName ? 'selected' : ''}
+                   onClick={() => this.select(cityName)}>{cityName}</button>;
   }
 
   render() {
@@ -22,7 +35,21 @@ export default class CitySelector extends Component {
           <li>{lives > 1 ? CitySelector.alive() : CitySelector.dead()}</li>
           <li>{lives > 2 ? CitySelector.alive() : CitySelector.dead()}</li>
         </ul>
+        <p className="question">Where do you want to travel?</p>
+        {this.state.showAbout ? <p className="description">We are going to give you questions about this city</p> : ''}
+        <div className="cities">
+          {this.button('Ica')}
+          {this.button('Trujillo')}
+          {this.button('Huaraz')}
+          {this.button('Arequipa')}
+          {this.button('Cusco')}
+          {this.button('Tarapoto')}
+        </div>
       </div>
     );
+  }
+
+  select(city) {
+    this.setState({city, showAbout: true});
   }
 }
